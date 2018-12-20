ab3a8dc14188dfd16efe7a786be0928eecc9e78a
Don Ward <10is0420@gmail.com>
2018-12-20 23:48:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.include?(brand) == 
-    BRANDS << brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
   
 
